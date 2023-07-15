81bc77154d70a182d55afe426c34533d6083d773
kBohdan <bkachmar1@gmail.com>
2023-07-15 19:18:51 +0300

    create left section main content

---
+++ dist/js/main.js
@@ -0,0 +1,2 @@
+
+document.querySelectorAll('.accordion-header').forEach(header=>{header.addEventListener('click',()=>{const item=header.parentElement;const content=item.querySelector('.accordion-content');if(content.style.maxHeight){content.style.maxHeight=null;}else{content.style.maxHeight=content.scrollHeight+'px';}});});
