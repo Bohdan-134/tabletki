2f00a68b850b0437b2c9bec37668a19ca50ded3a
kBohdan <bkachmar1@gmail.com>
2023-07-17 17:38:29 +0300

    add adaptive

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,29 @@
-document.querySelectorAll('.accordion-header').forEach(header => {
-    header.addEventListener('click', () => {
-        const item = header.parentElement;
-        const content = item.querySelector('.accordion-content');
+// accordion
+const headers = document.querySelectorAll('.accordion-header');
 
-        if (content.style.maxHeight) {
-            content.style.maxHeight = null;
-        } else {
-            content.style.maxHeight = content.scrollHeight + 'px';
-        }
+function toggleAccordion(header) {
+    const item = header.parentElement;
+    const content = item.querySelector('.accordion-content');
+    const isActive = content.style.maxHeight;
+
+    headers.forEach(({ classList, parentElement }) => {
+        parentElement.querySelector('.accordion-content').style.maxHeight = null;
+        classList.remove('active');
     });
+
+    if (!isActive) {
+        header.classList.add('active');
+        content.style.maxHeight = content.scrollHeight + 'px';
+    }
+}
+
+headers.forEach(header => header.addEventListener('click', () => toggleAccordion(header)));
+
+// open main menu
+const openMainContentBtn = document.getElementById('open-container-main');
+const bookingContainerMain = document.querySelector('.booking-container-main');
+
+openMainContentBtn.addEventListener('click', function() {
+    this.classList.toggle('active');
+    bookingContainerMain.classList.toggle('active');
 });
